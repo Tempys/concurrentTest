0c2fa76c63d350c052ce441dbae890b5e9804644
tempys <mikhailo.dubovskiy@outlook.com>
2016-10-15 10:35:40 +0300

    -add locks

---
+++ src/com/dubovskiy/concurrent/test/lock/ReentrantLockBufferTest.java
@@ -0,0 +1,47 @@
+package com.dubovskiy.concurrent.test.lock;
+
+/**
+ * Created by M on 15.10.2016.
+ */
+public class ReentrantLockBufferTest {
+    public static void main(String[] args) {
+
+        ReenrantLockBuffer<Integer> reentrantLockBufferTest = new <Integer>ReenrantLockBuffer(2);
+
+        Thread a =  new Thread(() ->{
+            try {
+                reentrantLockBufferTest.put(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        Thread b =  new Thread(() ->{
+            try {
+                reentrantLockBufferTest.put(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        Thread c =  new Thread(() ->{
+            try {
+                reentrantLockBufferTest.put(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+
+        a.start();
+        b.start();
+        c.start();
+
+
+
+
+    }
+}
