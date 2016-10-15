1d1c3ce09fba0c2209013720d5fb992e131929be
tempys <mikhailo.dubovskiy@outlook.com>
2016-10-15 10:32:24 +0300

    -add countdownlatchTest

---
+++ src/com/dubovskiy/concurrent/test/barrier/CountDownTest.java
@@ -0,0 +1,44 @@
+package com.dubovskiy.concurrent.test.barrier;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by M on 15.10.2016.
+ */
+public class CountDownTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(3);
+
+        Thread a = new Thread(() ->{
+            System.out.println("start a");
+            countDownLatch.countDown();
+
+        });
+
+
+        Thread b = new Thread(() ->{
+            System.out.println("start b");
+            countDownLatch.countDown();
+
+        });
+
+
+        Thread c = new Thread(() ->{
+            System.out.println("start c");
+            countDownLatch.countDown();
+
+        });
+
+        a.start();
+        b.start();
+        c.start();
+
+        countDownLatch.await();
+
+
+        System.out.println("main flow");
+
+
+    }
+}
